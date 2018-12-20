623dff79aaebbdd3063d82fc8ce0c2140ecd2f7d
G1nes <timursalakhov96@gmail.com>
2018-12-21 00:15:45 +0300

    Complete first task

---
+++ src/FifthTask/SecondTask.java
@@ -0,0 +1,12 @@
+package FifthTask;
+
+import java.util.Scanner;
+
+public class SecondTask {
+    public static void main (String [] args){
+        Scanner s= new Scanner(System.in);
+        String q = s.next();
+        int result=Integer.parseInt(q,2);
+        System.out.println(result);
+    }
+}

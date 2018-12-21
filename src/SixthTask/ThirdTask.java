a4f74a7b3dab949b15f6ce1d6d267f2de2e9ca6b
G1nes <6atYAakaTimur>
2018-12-21 18:50:31 +0300

    123

---
+++ src/SixthTask/ThirdTask.java
@@ -0,0 +1,13 @@
+package SixthTask;
+
+import java.util.Scanner;
+
+public class ThirdTask {
+    public static void main (String [] args){
+        Scanner s = new Scanner(System.in);
+        int q = s.nextInt();
+        if(q==1){
+            System.out.println("Вы ввели число 1");
+        }
+    }
+}

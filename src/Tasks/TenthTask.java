ecdcb6c805ad38f63ee66f24f852a9e597b58691
G1nes <6atYAakaTimur>
2018-12-21 22:48:03 +0300

    123

---
+++ src/Tasks/TenthTask.java
@@ -0,0 +1,11 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class TenthTask {
+    public static void main (String [] args){
+        Scanner s = new Scanner(System.in);
+        String q = s.nextLine();
+        System.out.println(q);
+    }
+}

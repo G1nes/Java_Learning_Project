6879ef1609dc10f72ae51cacf5c5a97105f756cf
G1nes <6atYAakaTimur>
2018-12-21 19:15:44 +0300

    123

--- src/Tasks/FourthTask.java
+++ src/Tasks/FourthTask.java
@@ -3,7 +3,7 @@ package Tasks;
 import java.util.Scanner;
 
 public class FourthTask {
-    public static void main () {
+    public static void main (String [] args) {
         int x=1, y=2,z=3;
         Scanner s = new Scanner(System.in);
         int q = s.nextInt();

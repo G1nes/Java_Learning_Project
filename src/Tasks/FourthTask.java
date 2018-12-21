f76ce72353a3ce7597b73ca6f12b29e57d94b1a1
G1nes <6atYAakaTimur>
2018-12-21 19:08:51 +0300

    123

--- src/Tasks/FourthTask.java
+++ src/Tasks/FourthTask.java
@@ -1,4 +1,16 @@
 package Tasks;
 
+import java.util.Scanner;
+
 public class FourthTask {
+    public static void main () {
+        int x=1, y=2,z=3;
+        Scanner s = new Scanner(System.in);
+        int q = s.nextInt();
+        if (q==x||q==y||q==z) {
+            System.out.println("Данное значение имеется в константах");
+        }else{
+            System.out.println("Такой константы нет!");
+        }
+    }
 }

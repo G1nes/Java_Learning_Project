6169eb535878f75bf3a20ce7d5bb7b1c610acccd
G1nes <6atYAakaTimur>
2018-12-21 23:03:08 +0300

    123

---
+++ src/Tasks/EleventhTask.java
@@ -0,0 +1,19 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class EleventhTask {
+    public static void main (String [] args){
+        Scanner s = new Scanner(System.in);
+        String q = s.nextLine();
+        char [] array = q.toCharArray();
+        String result="";
+        for (int i=0;i<array.length;i++){
+            if (array[i]==' '){
+                i=i+1;
+            }
+            result+=array[i];
+        }
+        System.out.println(result);
+    }
+}

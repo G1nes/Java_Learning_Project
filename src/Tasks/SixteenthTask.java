dd27ad803e83a4b438d0ca89fcad9f659ece08c1
G1nes <timursalakhov96@gmail.com>
2018-12-22 16:00:31 +0300

    Complete first task

---
+++ src/Tasks/SixteenthTask.java
@@ -0,0 +1,28 @@
+package Tasks;
+
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class SixteenthTask {
+    public static void main (String [] args) throws IOException {
+        String string;
+        Scanner sc = new Scanner(System.in);
+        try (FileWriter fos = new FileWriter("E:\\ссылки на задачи\\Задача 16.txt",true)) {
+             do {
+                 string = sc.nextLine();
+                 if (!string.equals("stop")){
+                     fos.write(string + "\r\n");
+                 }else{
+                     break;
+                 }
+             }
+            while(!string.equals("stop"));
+
+        }
+        catch (IOException e){
+            System.out.println("Ошибка");
+        }
+    }
+}

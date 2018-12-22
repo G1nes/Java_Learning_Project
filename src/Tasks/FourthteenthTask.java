c371e3388d5929cc122da7829ba56ada2bc5800d
G1nes <timursalakhov96@gmail.com>
2018-12-22 13:46:18 +0300

    Complete first task

---
+++ src/Tasks/FourthteenthTask.java
@@ -0,0 +1,28 @@
+package Tasks;
+
+public class FourthteenthTask {
+    public static void main (String [] args){
+        int [] list = {5,6,100,135,98,1,10};
+        showMeArray(bubbleSort(list));
+    }
+    public static int [] bubbleSort (int [] someArray){
+        int [] bubble = someArray;
+        int temp;
+        for (int i=bubble.length-1;i>0;i--){
+            for (int j=0;j<i;j++){
+                if (bubble[j]>bubble[j+1]){
+                    temp = bubble[j];
+                    bubble [j] = bubble [j+1];
+                    bubble [j+1] = temp;
+                }
+            }
+        }
+        return bubble;
+    }
+    public static void showMeArray (int [] someArray){
+        for (int i=0;i<someArray.length;i++){
+            System.out.print(someArray[i]+" ");
+        }
+    }
+}
+

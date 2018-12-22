193db58d8318a09a688c8c772d74be909ae9c22d
G1nes <timursalakhov96@gmail.com>
2018-12-22 13:03:25 +0300

    Complete first task

---
+++ src/Tasks/ThirdteenthTask.java
@@ -0,0 +1,15 @@
+package Tasks;
+
+import java.util.Scanner;
+
+public class ThirdteenthTask {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        String s = Integer.toString(sc.nextInt());
+        System.out.println(s);
+        int x = Integer.valueOf(s);
+        System.out.println(x);
+        double y = x;
+        System.out.println(y);
+    }
+}

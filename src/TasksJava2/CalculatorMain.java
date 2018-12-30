d56d51ef7b7ae73d5e078341b4bfa983decbf0da
G1nes <timursalakhov96@gmail.com>
2018-12-30 21:45:23 +0300

    Identification of operator

--- src/TasksJava2/CalculatorMain.java
+++ src/TasksJava2/CalculatorMain.java
@@ -8,29 +8,30 @@ import java.util.Scanner;
 public class CalculatorMain {
 
     private static String command;
+    private static Scanner s;
 
     public static void main (String [] args) throws IOException{
-        Scanner s = new Scanner(System.in);
+        s = new Scanner(System.in);
         System.out.println("Please, write command in format: \"a command b\"");
         command = s.nextLine();
         methodRecognition(stringToDoubleConverter(command));
     }
     private static void methodRecognition (Object [] array){
-        String operator = (String) array[2];
+        Character operator = (Character) array[2];
         switch (operator){
-            case "*":
+            case '*':
                 double a = (double) array [0] * (double) array[1];
                 System.out.println(a);
                 break;
-            case "/":
+            case '/':
                 double b = (double) array [0] / (double) array[1];
                 System.out.println(b);
                 break;
-            case "+":
+            case '+':
                 double c = (double) array [0] + (double) array[1];
                 System.out.println(c);
                 break;
-            case "-":
+            case '-':
                 double d = (double) array [0] - (double) array[1];
                 System.out.println(d);
                 break;
@@ -42,22 +43,28 @@ public class CalculatorMain {
     }
     private static Object [] stringToDoubleConverter (String method){
         Object [] array = new Object[3];
+        //Убираем пробелы
+        method = method.replaceAll("\\s+","");
+        //Делим строку на два числа относительно знака
+        String [] tmp;
+        tmp=method.split("\\+|\\-|\\*|\\/");
         try{
-            if (method.contains("*")){
-                array[2] ="*";
-            }else if (method.contains("/")){
-                array[2] ="/";
-            }else if (method.contains("+")) {
-                array[2] = "+";
-            }else if (method.contains("-")) {
-                array[2] = "-";
+        //Заносим числа и знак в массив
+        array[0] = Double.valueOf(tmp[0]);
+        array[1] = Double.valueOf(tmp[1]);
+        char result=0;
+        for (int i=0;i<method.length();i++){
+            if (method.charAt(i)=='+'||method.charAt(i)=='-'||method.charAt(i)=='*'||method.charAt(i)=='/'){
+                array[2]= result;
             }else{
-                System.out.println("Try again, use \"*\", \"/\", \"+\", \"-\" as operator" );
+                System.out.println("Try again, use \"*\", \"/\", \"+\", \"-\" as operator");
+                command = s.nextLine();
                 methodRecognition(stringToDoubleConverter(command));
             }
-
+        }
         }catch (NumberFormatException e){
             System.out.println("Please enter numbers");
+            command = s.nextLine();
             methodRecognition(stringToDoubleConverter(command));
         }
         return array;

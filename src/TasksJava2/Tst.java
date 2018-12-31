8da0fa330aa36d15f42164c2b559eda6504b829e
G1nes <timursalakhov96@gmail.com>
2018-12-31 17:31:27 +0300

    Identification of operator

---
+++ src/TasksJava2/Tst.java
@@ -0,0 +1,10 @@
+package TasksJava2;
+
+import java.io.IOException;
+
+public class Tst {
+    public static void main (String [] args) throws IOException {
+
+        CalculatorMain.main(args);
+    }
+}

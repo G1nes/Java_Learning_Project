907b90f09bd636d9553871ef78700f8483032f37
G1nes <timursalakhov96@gmail.com>
2019-01-01 22:50:46 +0300

    Identification of operator

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -1,9 +1,17 @@
 import TasksJava2.CalculatorTasks.Division;
 import org.junit.*;
 public class TestCalculator {
+    private static final float Infinity = 1/0f;
+
     @Test
     public void testOfDivisionFunction (){
         Division d1 = new Division();
         Assert.assertEquals(1,d1.division1(15.0,15.0),0);
+    }
+    @Test
+    public void testDivZero(){
+        Division d1 = new Division();
+        Assert.assertEquals(Infinity,d1.division1(15.0,0.0),0);
+        //Assert.assertEquals();
     }
 }
